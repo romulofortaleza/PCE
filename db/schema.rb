0b71f003e78e4ee3763e1b97a06ea5d5a3daedaf
Rômulo Fortaleza <romulofortaleza_@hotmail.com>
2018-09-03 11:25:04 -0300

    Conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_03_114430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
